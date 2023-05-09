9e4a9b16d12434eb99e9b434b0497f52aaa4bed1
John Doe <johndoe@example.com>
2023-05-09 11:27:20 -0400

    better map

---
+++ .history/src/views/LocationMap_20230509080237.vue
@@ -0,0 +1,62 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="col">
+        <h2>Island Craft Shop</h2>
+    <h3>329 US Route 2</h3>
+    <h3>South Hero, Vermont 05486</h3>
+    <a href="tel:8023723945">802 . 372 . 3945</a>
+    </div>
+    <div class="col">
+       <div v-if="new Date().getMonth() > 4">
+              <h2>{{new Date()}} WE ARE OPEN</h2>
+              <ul class="list-unstyled list-hours mb-5 text-left mx-auto">
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Sunday
+                                    <span class="ml-auto">10:00 AM to 4:00 PM</span>
+                               </li>
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Monday
+                                    <span class="ml-auto">10:00 AM to 4:00 PM</span>
+                                </li>
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Tuesday
+                                    <span class="ml-auto">10:00 AM to 4:00 PM</span>
+                                </li>
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Wednesday
+                                    <span class="ml-auto">10:00 AM to 4:00 PM</span>
+                                </li>
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Thursday
+                                    <span class="ml-auto">10:00 AM to 4:00 PM</span>
+                                </li>
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Friday
+                                    <span class="ml-auto">10:00 AM to 4:00 PM</span>
+                                </li>
+                                <li class="list-unstyled-item list-hours-item d-flex">
+                                    Saturday
+                                    <span class="ml-auto">10:00 AM to 4:00 PM</span>
+                                </li>
+                            </ul>
+                            </div>
+                            <div v-else-if="new Date().getMonth() === 12">
+                              This is for the holiday
+                            </div>
+                            <div v-else>
+                            Dead of winter
+                            </div>
+    </div>
+    </div>
+ 
+  <div class="row">
+    <div class="col">
+      <div id="map-container-google-3" class="z-depth-1-half map-container-3">
+  <iframe src="https://maps.google.com/maps?q=warsaw&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+    style="border:0" allowfullscreen></iframe>
+</div>
+    </div>
+  </div>
+</div>
+</template>
--- .history/src/views/LocationMap_20230509112534.vue
+++ .history/src/views/LocationMap_20230509112534.vue
@@ -7,7 +7,7 @@
 <div id="map-container" class="z-depth-1-half map-container" >
   <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d42650.00277284037!2d-73.32477164083456!3d44.64996967386753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sus!4v1683645847474!5m2!1sen!2sus" frameborder="0"
     style="border:0" allowfullscreen></iframe>
-   
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d42650.00277284037!2d-73.32477164083456!3d44.64996967386753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sus!4v1683645847474!5m2!1sen!2sus" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
 
         </div>
