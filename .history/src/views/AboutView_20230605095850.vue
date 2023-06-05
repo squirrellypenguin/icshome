05d3b508ff3c1cacbf6ccf7d9d7136034da4db3f
tedet <from@dabronx.nyc>
2023-06-05 10:14:42 -0400

    matted map on about

---
+++ .history/src/views/AboutView_20230605095850.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+<img class="mat" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/chinese-princess.jpg" alt="Photograph of a Chinese model dressed as a princess reclining on a wooden bench">
+</div>
+</template>
